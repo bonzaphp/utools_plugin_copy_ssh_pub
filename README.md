9160f656a020247a2e304f2e2f84a6752bb6f2aa
bonza <bonzaphp@gmail.com>
2023-06-07 10:52:26 +0800

    添加说明文件，提示只支持Windows系统

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+<!--
+ * @Author: bonza bonzaphp@gmail.com
+ * @Date: 2023-06-07 10:46:40
+ * @LastEditors: bonza bonzaphp@gmail.com
+ * @LastEditTime: 2023-06-07 10:50:49
+ * @FilePath: \utools\README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 # utools_plugin_copy_ssh_pub
 utools插件，快速拷贝本地公钥
+
+
+### 目前插件仅支持Windows系统
--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@
  * @Author: bonza bonzaphp@gmail.com
  * @Date: 2023-06-06 15:09:48
  * @LastEditors: bonza bonzaphp@gmail.com
- * @LastEditTime: 2023-06-07 10:34:10
+ * @LastEditTime: 2023-06-07 10:49:36
  * @FilePath: \utools\preload.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,22 +22,25 @@ window.exports = {
         mode: "none",
         args: {
             enter: (action) => {
-                // action = { code, type, payload }
-                // utools.copyText(action.payload)
-				const filePath = "c:/Users/Administrator/.ssh/id_rsa.pub"
-				fs.readFile(filePath, 'utf-8', (err, data) => {
-					if (err) {
-					  console.error(err);
-					  return;
-					}
-					clipboard.writeText(data);
-					// utools.showNotification('文件内容已复制到剪切板');
-				  });
-                // utools.copyText("ni zhe ge stupid")
-				// utools.copyFile('/c/Users/Administrator/.ssh/id_rsa.pub')
-                utools.outPlugin()
-                utools.hideMainWindow()
-                utools.showNotification('已复制')
+				if (utools.isWindows()) {
+					// action = { code, type, payload }
+					// utools.copyText(action.payload)
+					const filePath = "c:/Users/Administrator/.ssh/id_rsa.pub"
+					fs.readFile(filePath, 'utf-8', (err, data) => {
+						if (err) {
+						console.error(err);
+						return;
+						}
+						clipboard.writeText(data);
+						// utools.showNotification('文件内容已复制到剪切板');
+					});
+					// utools.copyText("ni zhe ge stupid")
+					// utools.copyFile('/c/Users/Administrator/.ssh/id_rsa.pub')
+					utools.outPlugin()
+					utools.hideMainWindow()
+					utools.showNotification('已复制')
+				}
+				utools.showNotification('插件不支持的系统')
             }
         }
     }
